4b067c707f4b2988c41b81d884b389b24e08fc3b
Jack Foley <j.foley11@nuigalway.ie>
2018-11-24 17:10:20 +0000

    Added global entropy calculation

--- C45Algorithm.py
+++ C45Algorithm.py
@@ -23,13 +23,45 @@ def calculateGlobalEntropy(data):
     return globalEntropy
 
 def calculateEntropy(data):
+    #need to do some sort of splitting to get ent and info gain
     i = 0
     x = len(list(data.head(0)))
-    #print(x)
+    dataFrameType = data.iloc[:, -1] #species/type
+    data = data.iloc[:, 0:x-1] #rest of data, to be split into continous and nominal
+    x = len(list(data.head(0)))
 
-    while i < x:
-        print("hello")
-        i += 1
+    continousOrNominal = {} #set that seperates the classes into continous or nominal
+    for row in data.columns:
+        continousOrNominal[row] = 1.*data[row].nunique()/data[row].count() < 0.05 #continous or nominal calculator
+
+    dataFrameContinous = pd.DataFrame() #creating the empty continous & nominal dataframes
+    dataFrameNominal = pd.DataFrame()
+    
+    for row in continousOrNominal.values():
+        if row == False:
+            dataFrameContinous = dataFrameContinous.append(data.iloc[:, i]) #adding to the dataframes
+            i += 1
+        else:
+            dataFrameNominal = dataFrameNominal.append(data.iloc[:, i])
+            i += 1
+
+    dataFrameContinous = dataFrameContinous.transpose() #transposing the dataframes so they are "flipped" the right way
+    datadataFrameNominal = dataFrameNominal.transpose()
+
+    testFrame = pd.DataFrame()
+    testFrame = testFrame.append(dataFrameContinous.iloc[:, 0])
+    testFrame = testFrame.append(dataFrameType)
+    testFrame = testFrame.transpose()
+    testFrame = testFrame.sort_values(testFrame.columns[0], ascending=True)
+
+    countOccurenecesOfValue = {} #set for the global entropy data
+    for row in (testFrame.iloc[:, 0]): #itterates through the last column in the data set
+        if row not in countOccurenecesOfValue: countOccurenecesOfValue[row] = 0 #adds the data to the set
+        countOccurenecesOfValue[row] += 1
+
+    for row in countOccurenecesOfValue.values()
+        p = float (row) / len(testFrame.iloc[:, 0])
+        #print(p)
 
 def main():
     df = pd.read_csv("owls.csv") #reading in the data
